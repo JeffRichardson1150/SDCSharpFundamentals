63eddb38d5f1930bd01323d2501bb107d9cf2b45
JeffRichardson1150 <jeffrichardson573@gmail.com>
2020-06-25 15:57:16 -0400

    2020-06-25 EOD

---
+++ 06_Classes/ClassExamples.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Classes
+{
+    public enum VehicleType { Car, Truck, Motorcycle, Spaceship, Plane, Boat, Submarine }
+    class Vehicle
+    {
+        // Properties : 
+        // [Access Modifer] [Type the Property can hold] [Property Name] { [Getters; & Setters;] }
+        // prop <tab> <tab>
+        public int MyProperty { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double Mileage { get; set; }
+        public string Color { get; set; }
+
+        public VehicleType TypeOfVehicle { get; set; }
+
+        // Empty Constructor.  Had to be created so we can create an empty Vehicle object in ClassTests
+        public Vehicle() { }
+
+        // Overloaded Constructor. When we create a Vehicle object, we can pass in all the parameters.
+        public Vehicle(string make, string model, int year, double mileage, string color, VehicleType tOV)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Mileage = mileage;
+            Color = color;
+            TypeOfVehicle = tOV;
+        }
+    }
+
+    public class Person
+    {
+        // Property
+        public string FirstName { get; set; }
+        // propfull <tab> <tab>
+        private string _lastName;
+
+        // Property
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        // Property which can't be set (only a getter) - composed from settable properties
+        public string FullName
+        { get { return $"{FirstName} {LastName}"; } }
+
+        // Property
+        public DateTime DateOfBirth { get; set; }
+
+        // Property which can't be set (only a getter) - composed from settable properties
+        public int AgeInYears
+        {
+            get
+            {
+
+                if (DateOfBirth == new DateTime())
+                {
+                    return 9001;
+                }
+                TimeSpan ageSpan = DateTime.Now - DateOfBirth;
+                double totalAgeInYears = ageSpan.TotalDays / 365.25;
+                return Convert.ToInt32(Math.Floor(totalAgeInYears));
+            }
+        }
+
+        // Property - use Vehicle as an object and use it as the transport property of a Person
+        public Vehicle Transport { get; set; }
+
+        // Constructor
+        public Person(string firstName, string lastName, DateTime birthday)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = birthday;
+        }
+    }
+}
---
+++ 06_Classes/ClassTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _06_Classes
+{
+    [TestClass]
+    public class ClassTests
+    {
+        [TestMethod]
+        public void VehiclePropertyTests()
+        {
+            Vehicle firstVehicle = new Vehicle();
+            firstVehicle.Color = "Blue";
+            Console.WriteLine(firstVehicle.Color);
+            firstVehicle.Make = "Cadillac";
+            firstVehicle.Model = "DTS";
+            firstVehicle.Mileage = 7;
+            firstVehicle.Year = 2018;
+            firstVehicle.TypeOfVehicle = VehicleType.Car;
+
+            string carDetails = $"{firstVehicle.Make} {firstVehicle.Color}";
+
+            Console.WriteLine(carDetails);
+
+            Vehicle myVehicle = new Vehicle();
+            myVehicle.Year = 2025;
+
+            Vehicle rocket = new Vehicle("SpaceX", "Falcon Heavy", 2015, 90000, "Space Gray", VehicleType.Spaceship);
+        }
+
+        // testm <tab> <tab> - create a new TestMethod
+        [TestMethod]
+        public void PersonTests()
+        {
+            Person personTwo = new Person("Appa", "YipYp", new DateTime(1805,06,11));
+            Console.WriteLine(personTwo.AgeInYears); ;
+            Console.WriteLine(personTwo.FullName);
+
+            Person newPerson = new Person("Zuko", "FireLord", DateTime(1990, 06, 22));
+            newPerson.Transport = new Vehicle("Pontiac", "Firebird", 1989, 100123, "Red & Gold", VehicleType.Car);
+            Console.WriteLine(newPerson.Transport.Model);
+
+            // The point of Unit Test Assemblies is to test the code you've written. A big part is the Assert.  Rather than - Run a test, Open Output, ... , with the Assert, we can run it and the green check will tell X amount of tests passed (in the Test Explorer)
+            // Here, we're confirming the FullName has been created properly from the data I provided.
+            Assert.AreEqual("Zuko FireLord", newPerson.FullName);
+        }
+    }
+}
